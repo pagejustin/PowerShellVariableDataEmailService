1646e7a42f6fa1da71b6c7956f17980cf4193d0c
Justin <justpage@gmail.com>
2023-07-01 16:37:49 -0400

    Add warning and rename PSMailer to PSMailer.ps1
    can you add a warning to the UI that explains the dangers of using this application and its security risks?

--- PSMailer.ps1
+++ PSMailer.ps1
@@ -55,8 +55,14 @@ $button3.Add_Click({
     }
 })
 
+$warningLabel=New-Object System.Windows.Forms.Label
+$warningLabel.Location=New-Object System.Drawing.Point(10,310)
+$warningLabel.Size=New-Object System.Drawing.Size(280,40)
+$warningLabel.Text="Warning: This application can be used to send emails to multiple recipients. Be careful when using this application and ensure that you have permission to send emails to the selected recipients."
+$form.Controls.Add($warningLabel)
+
 $button1 = New-Object System.Windows.Forms.Button
-$button1.Location = New-Object System.Drawing.Point(100,280)
+$button1.Location = New-Object System.Drawing.Point(100,340)
 $button1.Size = New-Object System.Drawing.Size(75,23)
 $button1.Text = "Send"
 $button1.DialogResult = [System.Windows.Forms.DialogResult]::OK
@@ -64,7 +70,7 @@ $form.AcceptButton = $button1
 $form.Controls.Add($button1)
 
 $button2 = New-Object System.Windows.Forms.Button
-$button2.Location = New-Object System.Drawing.Point(190,280)
+$button2.Location = New-Object System.Drawing.Point(190,340)
 $button2.Size = New-Object System.Drawing.Size(75,23)
 $button2.Text = "Cancel"
 $button2.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -79,8 +85,4 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
     foreach ($EmailAddress in $listbox1.SelectedItems) {
         $To=$EmailAddress.ToString()
         $Body=$BodyTemplate -replace "__FirstName__", $Recipient.FirstName -replace "__LastName__", $Recipient.LastName -replace "__Email__", $Recipient.EmailAddress
-        Send-MailMessage -To $To -From $From -Subject $textbox1.Text -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential (New-Object System.Management.Automation.PSCredential ($SMTPUsername, (ConvertTo-SecureString $SMTPPassword -AsPlainText -Force))) -BodyAsHtml
-    }
-}
-
-$form.Dispose()
+        Send-MailMessage -To $To -From $From -Subject $textbox1.Text -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort
